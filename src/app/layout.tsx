9d1d9ee11777612dda8afebbe58fc26b1e24fcda
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-10-31 13:08:56 -0500

    satoshi font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,11 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const satoshi = localFont({
+  src: "./fonts/Satoshi.ttf",
+  variable: "--font-satoshi",
+  weight: "400", // Specify weight here if known, otherwise omit or adjust as needed
+  style: "normal",
 });
 
 export const metadata: Metadata = {
@@ -25,10 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${satoshi.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-  return <div className='container'></div>;
+  return (
+    <div className='container w-3/5 p-4 border mx-auto flex flex-col gap-4'>
+      <h1 className='font-semibold text-center text-2xl'>
+        What would you like to know?
+      </h1>
+      <div className='px-8'>
+        <Input />
+      </div>
+    </div>
+  );
 }
