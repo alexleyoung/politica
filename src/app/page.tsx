46616adde8ae25666a69b8a10f220a062094b4e4
alexleyoung <139512340+alexleyoung@users.noreply.github.com>
2024-11-02 01:50:44 -0500

    move buttons around, edit base button styles, switch to textarea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { ArrowRight } from "lucide-react";
+import { Textarea } from "@/components/ui/textarea";
+import { ArrowRight, Layers3, Paperclip } from "lucide-react";
 
 export default function Home() {
   return (
@@ -10,17 +10,24 @@ export default function Home() {
       </h1>
       <div className='bg-accent flex flex-col p-2 gap-2 rounded-lg '>
         <div className='flex gap-2 items-center '>
-          <Input
-            className='border-none focus-visible:ring-0 shadow-none'
+          <Textarea
+            className='focus-visible:ring-0 shadow-none resize-none h-2 border-none text-base'
             placeholder="Summarize the current presidential candidates' platforms"
           />
-          <Button className='p-2 mr-3 size-6 bg-muted/80 hover:bg-muted rounded-full'>
+        </div>
+        <div className='flex justify-between items-center px-2'>
+          <div className='space-x-1'>
+            <Button variant='ghost' className='py-1 px-2 rounded-2xl'>
+              <Layers3 />
+              Focus
+            </Button>
+            <Button variant='ghost' className='py-1 px-2 rounded-2xl'>
+              <Paperclip /> Add
+            </Button>
+          </div>
+          <Button className='p-2 mr-3 size-7 bg-muted/80 hover:bg-muted hover:scale-110 rounded-full duration-300'>
             <ArrowRight className='text-primary' />
           </Button>
-        </div>
-        <div className='flex justify-between px-2'>
-          <Button variant='ghost'>Hello</Button>
-          <Button variant='ghost'>Hello</Button>
         </div>
       </div>
     </div>
